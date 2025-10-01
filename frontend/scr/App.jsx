d470d60b1da5c17557c08feceaecc20ef6d9ad27
Mikine197 <shekrubel754@gmail.com>
2025-10-01 20:11:27 +0600

    Add files via upload

--- frontend/scr/App.jsx
+++ frontend/scr/App.jsx
@@ -2,6 +2,9 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import Feed from "./pages/Feed";
+import Inbox from "./pages/Inbox";
+import Groups from "./pages/Groups";
+import MetaAIChat from "./pages/MetaAIChat";
 
 export default function App() {
   return (
@@ -10,6 +13,9 @@ export default function App() {
         <Route path="/" element={<Feed />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
+        <Route path="/inbox" element={<Inbox />} />
+        <Route path="/groups" element={<Groups />} />
+        <Route path="/meta" element={<MetaAIChat />} />
       </Routes>
     </BrowserRouter>
   );
